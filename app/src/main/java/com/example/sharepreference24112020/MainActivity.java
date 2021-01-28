c3aaff659e0dd026e319765cd8bd73893fba5d19
phatdroid94 <phatdroid94@gmail.com>
2021-01-28 18:49:35 +0700

    finish

---
+++ app/src/main/java/com/example/sharepreference24112020/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.sharepreference24112020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences mSharedPreferences;
+    SharedPreferences.Editor mEditor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSharedPreferences = getSharedPreferences("AppCache",MODE_PRIVATE);
+        String username = mSharedPreferences.getString("username","");
+        String password = mSharedPreferences.getString("password","");
+
+        Toast.makeText(this, username + " " + password, Toast.LENGTH_SHORT).show();
+//        mEditor = mSharedPreferences.edit();
+//        mEditor.putString("username","android24112020");
+//        mEditor.putString("password","123456");
+//        mEditor.apply();
+    }
+}
